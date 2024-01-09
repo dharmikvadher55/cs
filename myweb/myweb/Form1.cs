af14436fae70911fc6188846a6ccfb6780afd745
dharmik <dharmikdharmik535@gmail.com>
2024-01-10 00:15:29 +0530

    Add project files.

---
+++ myweb/myweb/Form1.cs
@@ -0,0 +1,15 @@
+namespace myweb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello", "title");
+        }
+    }
+}
